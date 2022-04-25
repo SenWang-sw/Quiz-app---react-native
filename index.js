5f00858313ef3d5051934864ea812c4ae02e4d4d
SenWang <72581932+wangsen1997@users.noreply.github.com>
2022-04-25 14:09:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/home';
+import Quiz from '../screens/quiz';
+import Result from '../screens/result';
+import Category from '../screens/category';
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} 
+      options={{headerShown: false}}/>
+      <Stack.Screen name="Quiz" component={Quiz} 
+      options={{headerShown: false}}/>
+      <Stack.Screen name="Result" component={Result} 
+      options={{headerShown: false}}/>
+      <Stack.Screen name="Category" component={Category} 
+      options={{headerShown: false}}/>
+    </Stack.Navigator>
+  );
+}
+
+export default MyStack;
